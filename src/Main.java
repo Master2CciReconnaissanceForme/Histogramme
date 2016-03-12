669735ee3d3c5ea4495119cd9f53e061a0962258
dadifamily <isabelle.dabat@free.fr>
2016-03-12 14:42:24 +0100

    Conversion en projet java dans eclipse

--- src/Main.java
+++ src/Main.java
@@ -1,37 +1,31 @@
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.LinkedList;
-
-import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.io.File;
+import java.net.URISyntaxException;
 
 import org.opencv.core.Core;
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfByte;
-import org.opencv.core.MatOfFloat;
-import org.opencv.core.MatOfInt;
-import org.opencv.core.Scalar;
-import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
-import org.opencv.imgproc.Imgproc;
 
 public class Main {
-	 static {
-		   System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-		 }
-	public static void main(String[] args) {
-		
-		Mat source = Highgui.imread("C:/Thermique.jpg");
-		Mat sourceGray = Highgui.imread("C:/Thermique.jpg", Highgui.CV_LOAD_IMAGE_GRAYSCALE);	
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+
+	public static void main(final String[] args) {
+
+		String pathFile = null;
+		try {
+			pathFile = new File(Main.class.getResource("Thermique.jpg").toURI()).getAbsolutePath();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Mat source = Highgui.imread(pathFile);
+		Mat sourceGray = Highgui.imread(pathFile, Highgui.CV_LOAD_IMAGE_GRAYSCALE);
 
 		Mat mask = TraitementImage.creerMask(source, 254, 255, 2);
 
 		Mat finale = TraitementImage.superposerMask(source, mask, true);
-		
+
 		Mat histImageSource = TraitementImage.creerHistogramme(source, 0, 256);
 		Mat histImageSourceGray = TraitementImage.creerHistogramme(sourceGray, 0, 256);
 		Mat histImageFinale = TraitementImage.creerHistogramme(finale, 1, 256);
@@ -41,7 +35,7 @@ public class Main {
 
 		TraitementImage.afficherImage(sourceGray);
 		TraitementImage.afficherImage(histImageSourceGray);
-		
+
 		TraitementImage.afficherImage(finale);
 		TraitementImage.afficherImage(histImageFinale);
 	}
