aada3030e85743a975f8eee0f5c4d912020cb2fc
BuildTools <jonathan.navarro@etu.univ-amu.fr>
2016-03-15 19:51:34 +0100

    Alpha 0.0.1

--- src/Main.java
+++ src/Main.java
@@ -11,27 +11,7 @@ public class Main {
 		   System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		 }
 	public static void main(String[] args) {
-		/*
-		Mat source = Highgui.imread("C:/Thermique.jpg");
-		Mat sourceGray = Highgui.imread("C:/Thermique.jpg", Highgui.CV_LOAD_IMAGE_GRAYSCALE);	
 
-		Mat mask = TraitementImage.creerMask(source, 254, 255, 2);
-
-		Mat finale = TraitementImage.superposerMask(source, mask, true);
-		
-		Mat histImageSource = TraitementImage.creerHistogramme(source, 0, 256);
-		Mat histImageSourceGray = TraitementImage.creerHistogramme(sourceGray, 0, 256);
-		Mat histImageFinale = TraitementImage.creerHistogramme(finale, 1, 256);
-
-		TraitementImage.afficherImage(source);
-		TraitementImage.afficherImage(histImageSource);
-
-		TraitementImage.afficherImage(sourceGray);
-		TraitementImage.afficherImage(histImageSourceGray);
-		
-		TraitementImage.afficherImage(finale);
-		TraitementImage.afficherImage(histImageFinale);*/
-		
 		String selectedDate = "2016-03-15";
 		Date date = new Date();
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
---
+++ src/modelProjet/Masques.java
@@ -0,0 +1,5 @@
+package modelProjet;
+
+public class Masques {
+
+}
